37d577ff5c7992c136c5d2ed78d54955e0e3495a
2power9 <huenhu0512@gmail.com>
2022-06-06 15:09:43 +0700

    feat: add assignment 2 and assignment 3

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # module-02-backend-thinking
-ZaloPay Fresher2022
+
+## Assignment 1️⃣
+
+## Assignment 2️⃣ 
+
+Requirement: customize and demo terminal
+
+| ![](https://i.imgur.com/nQhC5xT.png) |
+|:---:|
+| Demo using `fzf`|
+
+
+|![](https://i.imgur.com/1UThqfB.png)|
+|:---:|
+| Yah.. maybe in some case I can show this terminal to my buddy when having bugs |
+
+|![](https://i.imgur.com/KMNtFCe.jpg)|
+|:---:|
+| Demo using `zsh` |
+
+## Assignment 3️⃣
+
+### Requirement
+- Read a config file, find the current version appear in the find (there will be only one), print it out
+- Try to "bump" the version to the next:
+  - Patch version 
+  - Minor version 
+  - Major version
+- Upload your script to your vps
+
+#### Input:
+- Part of the version you need to increase
+- Input file that contains the version string
+
+#### Output:
+That input file but with version string modified
+
+## Assignment 4️⃣
+
+## Assignment 5️⃣
---
+++ pumpVersion.py
@@ -0,0 +1,64 @@
+from sys import argv
+
+
+def printUsage():
+    print("Usage: python3 [--major] [--minor] [--patch] [filename]")
+    exit(2)
+
+def checkCmdLine():
+    # check the command line arguments
+    if len(argv) < 3: printUsage()
+    for i in range(1, len(argv) - 1):
+        if argv[i] not in ['--major', '--minor', '--patch']:
+            printUsage()
+
+def getVersion(line):
+    posBeginVer = posEndVer = 0
+
+    for posBeginVer in range(len(line)):
+        if '0' <= line[posBeginVer] <= '9': break
+
+    for i in range(posBeginVer + 1, len(line)):
+        if not(line[i] == '.' or '0' <= line[i] <= '9'):
+            posEndVer = i
+            break
+
+    version = line[posBeginVer:posEndVer]
+    if len(version.split('.')) < 3: return ''
+    return version
+
+def pump(oldVersion):
+    parts = list(map(int, oldVersion.split('.')))
+    major, minor, patch = parts[0], parts[1], parts[2]
+    for i in range(1, len(argv) - 1):
+        if argv[i] == '--major': major += 1
+        elif argv[i] == '--minor': minor += 1
+        elif argv[i] == '--patch': patch += 1
+
+    newVersion = "%d.%d.%d" % (major, minor, patch)
+    return newVersion
+
+def processFile(filename):
+    try:
+        data = None
+        # read file to find version
+        with open(filename, 'r') as file:
+            data = file.readlines()
+
+            for i in range(len(data)):
+                version = getVersion(data[i])
+                if version != '':
+                    data[i] = data[i].replace(version, pump(version))
+                    break
+        # write file with the new version
+        with open(filename, 'w') as file:
+            for line in data:
+                file.writelines(line)
+
+    except IOError:
+        print("Error 404: file not found")
+        return
+
+if __name__ == '__main__':
+    checkCmdLine()
+    processFile(argv[-1])
