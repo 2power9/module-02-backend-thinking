c268f8c8d6dc6943cb6dd4459a6f81fa0fb63687
2power9 <huenhu0512@gmail.com>
2022-06-07 15:08:17 +0700

    feat: change code find version using regex

--- Assignment4/bumpVersion.py
+++ Assignment4/bumpVersion.py
@@ -1,31 +1,23 @@
 from sys import argv
+from re import compile
 
 
 def printUsage():
     print("Usage: python3 [--major] [--minor] [--patch] [filename]")
     exit(2)
 
-def checkCmdLine():
+
+def checkCommandLine():
     # check the command line arguments
     if len(argv) < 3: printUsage()
     for i in range(1, len(argv) - 1):
         if argv[i] not in ['--major', '--minor', '--patch']:
             printUsage()
 
-def getVersion(line):
-    posBeginVer = posEndVer = 0
 
-    for posBeginVer in range(len(line)):
-        if '0' <= line[posBeginVer] <= '9': break
+def isValidVersion(version):
+    return len(list(version.split('.'))) >= 3
 
-    for i in range(posBeginVer + 1, len(line)):
-        if not(line[i] == '.' or '0' <= line[i] <= '9'):
-            posEndVer = i
-            break
-
-    version = line[posBeginVer:posEndVer]
-    if len(version.split('.')) < 3: return ''
-    return version
 
 def bump(oldVersion):
     parts = list(map(int, oldVersion.split('.')))
@@ -37,32 +29,39 @@ def bump(oldVersion):
         elif argv[i] == '--minor':
             minor += 1
             patch = 0
-        elif argv[i] == '--patch': patch += 1
+        elif argv[i] == '--patch':
+            patch += 1
 
     newVersion = "%d.%d.%d" % (major, minor, patch)
     return newVersion
 
-def processFile(filename):
+
+def bumpVersionInFile(filename):
     try:
-        data = None
         # read file to find version
         with open(filename, 'r') as file:
-            data = file.readlines()
+            text = ''.join(file.readlines())
 
-            for i in range(len(data)):
-                version = getVersion(data[i])
-                if version != '':
-                    data[i] = data[i].replace(version, bump(version))
-                    break
+        # find valid version
+        version = ""
+        regex = compile(r'([\d][.\d]+)')
+        try: version = regex.search(text).group()
+        except AttributeError or not isValidVersion(version):
+            print("This file does not have version.")
+            return
+
+        # bump version in file
+        text = text.replace(version, bump(version))
+
         # write file with the new version
         with open(filename, 'w') as file:
-            for line in data:
-                file.writelines(line)
+            file.write(text)
 
     except IOError:
         print("Error 404: file not found")
         return
 
+
 if __name__ == '__main__':
-    checkCmdLine()
-    processFile(argv[-1])
+    checkCommandLine()
+    bumpVersionInFile(argv[-1])
